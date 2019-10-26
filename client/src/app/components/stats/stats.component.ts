d7ce7512dd732c790edbb6a3a069949f491d7544
vinson aiono <vinson.aiono@gmail.com>
2019-10-26 14:49:29 -0700

    updated charts on stats page

--- client/src/app/components/stats/stats.component.ts
+++ client/src/app/components/stats/stats.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+
 
 @Component({
   selector: 'app-stats',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stats.component.css']
 })
 export class StatsComponent implements OnInit {
+  LineChart:Array<Object> = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.LineChart = new Chart('lineChart', {
+      type : 'line',
+      data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'Current Week',
+            //backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 50, 20, 20, 30, 32]
+        }, 
+        {
+          label: 'Previous Week',
+          //backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(25, 99, 132)',
+          data: [10, 0, 30, 40, 20, 35, 22]
+      }]
+    },
+    })
   }
 
 }
